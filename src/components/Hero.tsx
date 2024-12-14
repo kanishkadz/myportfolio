2f8b14265bf705320fe9ec71be51c4ec3e6319cb
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-14 18:27:52 +0530

    Create Hero.tsx

---
+++ src/components/Hero.tsx
@@ -0,0 +1,15 @@
+"use client";
+import Image from 'next/image';
+import cursor from "../assets/icon1.png";
+import message from "../assets/icon2.png";
+import { motion } from 'framer-motion';
+import profilepic from "../assets/profilepic.png";
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>Hero</div>
+  )
+}
+
+export default Hero
