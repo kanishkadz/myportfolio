d31734122fdac3e79e6d3706790bf37e7da67133
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-14 18:37:27 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
         <div>
             <div className='text-8xl font-bold text-center'>
                 <h1 className='text-[#98B4CE]'>Hi, I am</h1>
-                <h1>Kanishka Anand</h1>
+                <h1 className='text-[#E48A57]'>Kanishka Anand</h1>
             </div>
         </div>
     </div>
