a4dd085350b8686fa3d2572f81ad54c66e8e6c93
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-14 17:44:35 +0530

    Update Navbar.tsx

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,5 @@
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { motion } from 'framer-motion';
