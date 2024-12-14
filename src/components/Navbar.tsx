4e35a64c12f121b341e8e5c37f085a3d03f487dd
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-14 18:07:45 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -24,7 +24,7 @@ const Navbar = () => {
     return (
         <div className="text-white/70 py-6">
             <div className="hidden md:flex items-center px-4 py-2 mx-auto max-w-[400px]">
-                <ul>
+                <ul className="flex flex-row p-4 space-x-8">
                     {navLinks.map((link, index) => (
                         <li key={index} >
                             <Link href={link.path}>
@@ -42,6 +42,10 @@ const Navbar = () => {
                         </a>
                     </li>
                 </ul>
+            </div>
+
+            <div>
+                {nav ? '' : <AiOutlineMenu size={30} />}
             </div>
 
         </div>
