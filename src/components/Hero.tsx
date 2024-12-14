a61a751e072eb2969fad8ea1a6b0a4dfbd1f8ee5
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-14 18:43:38 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -30,7 +30,7 @@ const Hero = () => {
             </motion.div>
         </div>
 
-        <p>
+        <p className='text-center text-xl max-w[500px' mx-auto>
             I am a full-stack developer focused on creating websites that rpovide the best experience for users.
         </p>
 
