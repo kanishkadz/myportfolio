690cfbbaeaa5337d5a5970cafeba7a855a194922
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-15 15:49:55 +0530

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -23,6 +23,26 @@ const About = () => {
           </div>
         </div>
 
+        <div className='col-span-3 relative bg-white/10 backdrop-blur-lg border border-white/20 rounded-xl overflow-hidden'>
+          <div className='flex flex-row p-6'>
+            <Image src={book} alt='book' className='w-auto h-[130px]' />
+            <div className='flex flex-col mt-2'>
+              <h2 className='text-2xl font-bold tetx-white/80'>xxxx</h2>
+              <p className='text-lg text-white/70 mt-2'>xxxx</p>
+            </div>
+          </div>
+        </div>
+
+        <div className='col-span-3 relative bg-white/10 backdrop-blur-lg border border-white/20 rounded-xl overflow-hidden'>
+          <div className='flex flex-row p-6'>
+            <Image src={book} alt='book' className='w-auto h-[130px]' />
+            <div className='flex flex-col mt-2'>
+              <h2 className='text-2xl font-bold tetx-white/80'>xxxx</h2>
+              <p className='text-lg text-white/70 mt-2'>xxxx</p>
+            </div>
+          </div>
+        </div>
+
         <div className='col-span-5 relative bg-white/10 backdrop-blur-lg border border-white/20 rounded-xl overflow-hidden'>
           <div className='flex flex-row p-6'>
             <Image src={book} alt='book' className='w-auto h-[130px]' />
