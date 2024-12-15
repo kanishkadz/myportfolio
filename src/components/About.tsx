fdb87ceb1f75319d4d3b18d60f7c8cf29ab3ff3b
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-15 15:48:49 +0530

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -15,13 +15,13 @@ const About = () => {
 
 
         <div className='col-span-5 relative bg-white/10 backdrop-blur-lg border border-white/20 rounded-xl overflow-hidden'>
-        <div className='flex flex-row p-6'>
-          <Image src={book} alt='book' className='w-auto h-[130px]' />
-          <div className='flex flex-col'>
-            <h2 className='text-2xl font-bold tetx-white/80'>xxxx</h2>
-            <p className='text-lg text-white/70 mt-2'>xxxx</p>
+          <div className='flex flex-row p-6'>
+            <Image src={book} alt='book' className='w-auto h-[130px]' />
+            <div className='flex flex-col mt-2'>
+              <h2 className='text-2xl font-bold tetx-white/80'>xxxx</h2>
+              <p className='text-lg text-white/70 mt-2'>xxxx</p>
+            </div>
           </div>
-        </div>
 
         </div>
 
