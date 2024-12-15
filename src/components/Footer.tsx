8e1aa492765576e43e075588395e4c5b5d59ae85
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-15 22:37:58 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,6 +3,8 @@ import { FaLinkedin, FaTwitter, FaInstagram } from 'react-icons/fa'
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='mt-12 text-white/70 py-8 container mx-auto border-t border-gray-700 pt-4 flex justify-between items-center'>
+
+    </div>
   )
 }
