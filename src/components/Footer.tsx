e48d9697b90bd438796a566238ed16bc5bb89bdc
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-15 22:39:02 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,12 @@ import { FaLinkedin, FaTwitter, FaInstagram } from 'react-icons/fa'
 export const Footer = () => {
   return (
     <div className='mt-12 text-white/70 py-8 container mx-auto border-t border-gray-700 pt-4 flex justify-between items-center'>
+        <h1 className='text-2xl font-bold'>Kanishka Anand</h1>
+        <div>
+            <a href="#" className='hover:text-gray-300'>
 
+            </a>
+        </div>
     </div>
   )
 }
