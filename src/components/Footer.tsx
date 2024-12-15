04bba98bb99a924ff10e74b67755d15d8b3766ff
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-15 22:40:47 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,9 @@ import { FaLinkedin, FaTwitter, FaInstagram } from 'react-icons/fa'
 
 export const Footer = () => {
   return (
-    <div className='mt-12 text-white/70 py-8 max-w-[800px] mx-auto border-t border-gray-700 pt-4 flex justify-between items-center'>
+    <div className='mt-12 text-white/70 py-8 max-w-[1000px] mx-auto border-t border-gray-700 pt-4 flex justify-between items-center'>
         <h1 className='text-2xl font-bold'>Kanishka Anand</h1>
-        <div>
+        <div className='flex space-x-6 mt-4'>
             <a href="#" className='hover:text-gray-300'>
                 <FaLinkedin size={24} />
             </a>
