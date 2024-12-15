65126210cb92c2b5130e90ef0c5845c45675263b
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-15 12:42:49 +0530

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,4 +1,11 @@
-import React from 'react'
+"use client";
+import React from 'react';
+import Image from "next/image";
+import aboutImg from "../assets/profilepic.png";
+import book from "../assets/book.png";
+import pc from "../assets/pc.png";
+import card from "../assets/card.png";
+import finance from "../assets/finance.png";
 
 
 const About = () => {
