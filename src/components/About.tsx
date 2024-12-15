7e2d6a6fd0487afa544d5f105a22f8828b8457ab
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-15 12:48:17 +0530

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -10,7 +10,16 @@ import finance from "../assets/finance.png";
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='max-w-[1200px] mx-auto' id='about'>
+      <div className='grid grid-cols-8 ga-6 place-items-center'>
+
+
+        <div className='col-span-5 relative bg-white/10 backdrop-blur-lg border border-white/20 rounded-xl overflow-hidden'>
+
+        </div>
+
+      </div>
+    </div>
   )
 }
 
