b69d75b61f50506049949bb57fef37836aa8edcd
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-15 17:39:27 +0530

    Create Skills.tsx

---
+++ src/components/Skills.tsx
@@ -0,0 +1,14 @@
+import About from "@/components/About";
+import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
+import Portfolio from "@/components/Portfolio";
+import Image from "next/image";
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <div>Skills</div>
+  )
+}
+
+export default Skills
