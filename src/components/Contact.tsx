e9fd4eebeceba7d01efaf5b68287916535b45f95
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-15 22:18:19 +0530

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { FaLinkedin, FaTwitter, FaInstagram } from 'react-icons/fa'
+import Image from "next/image";
+import phone from "../assets/phone.png";
+import mail from "../assets/mail.png";
 
 const Contact = () => {
   return (
