94fc24c702e19839601fd2ffa7c795cb0f587d20
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-15 17:58:15 +0530

    Update Skills.tsx

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -11,9 +11,9 @@ const skillIcons = [
 const Skills = () => {
   return (
     <div className='bg-[linear-gradient(to_top, #000, #381a5f_80%)] py-32'>
-      <div className='text-white w-[400px] md:max-w-[950px] mx-auto p-8 text-center'>
+      <div className='text-white w-[400px] max-w-[950px] mx-auto p-8 text-center'>
         <h2 className='text-6xl font-bold mb-4'>What I do</h2>
-        <div className='grid grid-cols-2 md:grid-cols-4 space-x-4'>
+        <div className='grid grid-cols-2 md:grid-cols-4 gap-6'>
           {skillIcons.map((skill, index) => (
             <div key={index} className='h-[160px] w-[160px] md:h-[220px] md:w-[220px] flex flex-col justify-between items-center bg-white/10 p-4 rounded-xl'>
               {skill.icon}
