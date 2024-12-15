23649415f24c42027b5723d6b420074525c4fa04
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-15 17:44:08 +0530

    Update Skills.tsx

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -3,7 +3,9 @@ import { FaHtml5, FaCss3Alt, FaReact, FaJsSquare } from "react-icons/fa";
 
 const skillIcons = [
     { icon: <FaHtml5 size={140} />, label: "HTML" },
-    { icon: <FaHtml5 size={140} />, label: "CSS" },
+    { icon: <FaCss3Alt size={140} />, label: "CSS" },
+    { icon: <FaReact size={140} />, label: "React" },
+    { icon: <FaJsSquare size={140} />, label: "JavaScript" },
 ]
 
 const Skills = () => {
