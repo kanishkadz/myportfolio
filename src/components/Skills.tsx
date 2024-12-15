3d7da0430ff27cd6e074ce14c67a24b923a2e15c
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-15 17:39:52 +0530

    Update Skills.tsx

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,8 +1,3 @@
-import About from "@/components/About";
-import Hero from "@/components/Hero";
-import Navbar from "@/components/Navbar";
-import Portfolio from "@/components/Portfolio";
-import Image from "next/image";
 import React from 'react'
 
 const Skills = () => {
