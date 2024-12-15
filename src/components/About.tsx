2b8bc09aa87d2e4ed03d50cd00f7f33fb31d34da
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-15 15:49:16 +0530

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -13,6 +13,15 @@ const About = () => {
     <div className='max-w-[1200px] mx-auto' id='about'>
       <div className='grid grid-cols-8 ga-6 place-items-center'>
 
+        <div className='col-span-5 relative bg-white/10 backdrop-blur-lg border border-white/20 rounded-xl overflow-hidden'>
+          <div className='flex flex-row p-6'>
+            <Image src={book} alt='book' className='w-auto h-[130px]' />
+            <div className='flex flex-col mt-2'>
+              <h2 className='text-2xl font-bold tetx-white/80'>xxxx</h2>
+              <p className='text-lg text-white/70 mt-2'>xxxx</p>
+            </div>
+          </div>
+        </div>
 
         <div className='col-span-5 relative bg-white/10 backdrop-blur-lg border border-white/20 rounded-xl overflow-hidden'>
           <div className='flex flex-row p-6'>
@@ -22,7 +31,6 @@ const About = () => {
               <p className='text-lg text-white/70 mt-2'>xxxx</p>
             </div>
           </div>
-
         </div>
 
       </div>
