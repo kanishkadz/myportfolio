974c014d9881c6d630d89095b4e248227ee4ed01
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-15 16:02:40 +0530

    Create Portfolio.tsx

---
+++ src/components/Portfolio.tsx
@@ -0,0 +1,14 @@
+"use client";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import project from "../assets/project.jpg";
+import project1 from "../assets/project.jpg";
+import React from 'react'
+
+const Portfolio = () => {
+  return (
+    <div>Portfolio</div>
+  )
+}
+
+export default Portfolio
